6b278201d6314e6e11c0d3ab36490f3885fe7e02
Giuseppe <giuseppedf8711@gmail.com>
2020-09-28 17:04:46 +0200

    template

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 $(document).ready (function() {
 
+// ready
+
+  $.ajax(
+    {
+      "url": "http://157.230.17.132:3018/todos",
+      "method": "GET",
+      "succes": function(data){
+        console.log(data);
+
+      },
+
+      "error": function () {
+        alert("errore");
+      }
+    }
+  );
 
 });
+
+function render() {
+  var source = $("#elm-template").html();
+  var template = Handlebars.compile(source);
+
+
+
+  var html = template(data);
+
+  $("#list").append(html);
+
+
+
+}
